bd7fdd1b0db62eaa0edb3c1e2c22625004fa62a1
ilyasahmedshaikh <ilyasahmed081@gmail.com>
2020-11-21 13:25:17 +0500

    Updates

---
+++ precache-manifest.0e804e9078e85a81f18d2a0f3f788b7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e5b40ed253cbb373394d08a7f1483f3",
+    "url": "/react-boilerplate/index.html"
+  },
+  {
+    "revision": "a734b8b2d20fdf4c04e8",
+    "url": "/react-boilerplate/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5c6111bf3e1d166104aa",
+    "url": "/react-boilerplate/static/css/main.fdb71764.chunk.css"
+  },
+  {
+    "revision": "a734b8b2d20fdf4c04e8",
+    "url": "/react-boilerplate/static/js/2.51b8ec7e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-boilerplate/static/js/2.51b8ec7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c6111bf3e1d166104aa",
+    "url": "/react-boilerplate/static/js/main.9f93d170.chunk.js"
+  },
+  {
+    "revision": "fb4e6ea203dbf893e51e",
+    "url": "/react-boilerplate/static/js/runtime-main.b9062b8e.js"
+  }
+]);
