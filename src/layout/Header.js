a41ae12353b6770e81d29503c7c4e57646ebea98
ilyasahmedshaikh <ilyasahmed081@gmail.com>
2020-11-21 15:01:00 +0500

    responsive header added

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import '../assets/css/header.css'
 // import logo from '../assets/img/logo_465x320.png'
 
 function Header() {
+
+    const [menuToggle, setMenuToggle] = useState(false);
+
     return(
         <div>
             <div className="header">
-                <div className="container">
-                    <div className="row">
 
+                <div className={"mobile-menu-toggle " + (menuToggle ? 'change' : '')} onClick={() => setMenuToggle(!menuToggle)}>
+                    <div className="bar1"></div>
+                    <div className="bar2"></div>
+                    <div className="bar3"></div>
+                </div>
+
+                <div className="desktop-menu container">
+                    <div className="row">
                         <div className="col-sm-4 col-md-4 col-lg-4">
                             <div className='logo'>
-                                {/* <img src={logo} /> */}
-                                <a href="#">Logo</a>
+                                <a href="#"><i class="fas fa-dice-d20"></i>ReactJS</a>
                             </div>
                         </div>
 
@@ -28,8 +36,22 @@ function Header() {
                                  </ul>
                             </div>
                         </div>
-                       
                     </div>
+                </div>
+
+                <div className={"mobile-menu sidenav " + (menuToggle ? 'open' : 'close')}>
+
+                    <div className="col-sm-4 col-md-4 col-lg-4">
+                        <div className='logo'>
+                            <a href="#"><i class="fas fa-dice-d20"></i></a>
+                        </div>
+                    </div>
+
+                    <li><Link to={process.env.PUBLIC_URL + '/home'} onClick={() => setMenuToggle(!menuToggle)}>Home</Link></li>
+                    <li><Link to={process.env.PUBLIC_URL + '/services'} onClick={() => setMenuToggle(!menuToggle)}>Services</Link></li>
+                    <li><Link to={process.env.PUBLIC_URL + '/about'} onClick={() => setMenuToggle(!menuToggle)}>About</Link></li>
+                    <li><Link to={process.env.PUBLIC_URL + '/contact'} onClick={() => setMenuToggle(!menuToggle)}>Contact</Link></li>
+                    <li><Link to={process.env.PUBLIC_URL + '/login'} onClick={() => setMenuToggle(!menuToggle)}>Login</Link></li>
                 </div>
             </div>
         </div>
